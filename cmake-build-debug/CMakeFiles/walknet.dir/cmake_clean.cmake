3a4fd11e5d4320649d6c3299a950d4704f1d8be9
iloveooz <iloveooz@yandex.ru>
2019-01-21 15:35:21 +0700

    started working

---
+++ cmake-build-debug/CMakeFiles/walknet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/walknet.dir/main.cpp.o"
+  "walknet.pdb"
+  "walknet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/walknet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
